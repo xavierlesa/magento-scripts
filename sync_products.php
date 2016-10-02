fa414fd8fada2ca841c03a63977987dafbc889c5
Xavier <xavierlesa@gmail.com>
2016-10-02 09:59:27 -0300

    add mapping images to csv

--- sync_products.php
+++ sync_products.php
@@ -487,7 +487,7 @@ class CommandUtilMagento
         foreach($array_images_files as $pimg)
         {
             $array_path = pathinfo($pimg);
-            $campos = explode('_', $array_path['filename']) + array($array_path['basename'], $pimg);
+            $campos = array_merge(explode('_', $array_path['filename']), array($array_path['basename'], $pimg));
             // codigo_producto, codigo_color, path
             fputcsv($fp, $campos);
         }
